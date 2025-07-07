365eebb9a86a83ba84da3fcdf77255bdaa65569c
rajithagumma <111038473+rajithagumma@users.noreply.github.com>
2025-07-07 23:22:57 +0530

    Update taiga-install.sh

--- taiga-install.sh
+++ taiga-install.sh
@@ -28,7 +28,6 @@ echo \
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker $USER
-newgrp docker
 
 # ------------------ CLONE TAIGA ------------------
 if [ ! -d "taiga-docker" ]; then
